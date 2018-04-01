cda88a94b7bb5170f8f60266a6838bd898241faf
balazs.saros <balazs.saros@gmail.com>
2018-04-01 08:18:34 +0200

    command line options support

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
 
+function initConfig(){
+    return {
+        wordsPerLine: parseInt(argvParser(['-w', '--words'], '9'), 10),
+        givenSeconds: parseInt(argvParser(['-t', '--time'], '60'), 10),
+        inputFile: argvParser(['-i', '--input'], 'data/mostCommon1000.txt')
+    }
+}
+
+function argvParser(flags, dflt){
+    for(flag of flags){
+        if(process.argv.indexOf(flag) != -1) {
+            return process.argv[process.argv.indexOf(flag) + 1];
+        }
+    }
+    return dflt;
+}
+
+function* lineGenerator(path, k) {
+    const words = fs.readFileSync(path, 'utf8').split('\n');
+    // shuffle the array (from: https://gist.github.com/guilhermepontes/17ae0cc71fa2b13ea8c20c94c5c35dc4)
+    const shuffledWords = words.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
+    for(let i = 0; i+k < shuffledWords.length-1; i += k) {
+        yield shuffledWords.slice(i, i + k).join(' ');
+    }
+}
+
 const SPECIAL = {
     CTRL_C: '\u0003',
     BACKSPACE: '\u007f',
@@ -15,32 +41,7 @@ const SPECIAL = {
     BG_END: '\x1b[0m'
 }
 
-function random(min, max) {
-    if (max == null) {
-        max = min;
-        min = 0;
-    }
-    return min + Math.floor(Math.random() * (max - min + 1));
-};
-
-function shuffle(obj) {
-    let sample = obj.slice();
-    const last = sample.length - 1;
-    for (let index = 0; index < sample.length; index++) {
-        let rand = random(index, last);
-        let temp = sample[index];
-        sample[index] = sample[rand];
-        sample[rand] = temp;
-    }
-    return sample.slice();
-};
-
-function* lineGenerator(path, k) {
-    const shuffledWords = shuffle(fs.readFileSync(path, 'utf8').split('\n'));
-    for(let i = 0; i+k < shuffledWords.length-1; i += k) {
-        yield shuffledWords.slice(i, i + k).join(' ');
-    }
-}
+const config = initConfig();
 
 // the upper text which shows what to type
 let results = '';
@@ -48,14 +49,14 @@ let results = '';
 let wrote = '';
 let started = false;
 let startTime;
-let givenSeconds = 6;
-let wordsPerLine = 9;
 
-let corrects = 0;
-let errors = 0;
-let keypresses = 0;
+let stats = {
+    corrects: 0,
+    errors: 0,
+    keypresses: 0
+}
 
-const lineGen = lineGenerator('data/mostCommon1000.txt', wordsPerLine);
+const lineGen = lineGenerator(config.inputFile, config.wordsPerLine);
 
 let text = lineGen.next().value;
 let nextText = lineGen.next().value;
@@ -64,27 +65,27 @@ process.stdout.write(text + '\n' + nextText + '\n\n');
 let cursor = 0;
 
 stdin.on('data', key => {
+    // exit on ctrl-c
+    if (key == SPECIAL.CTRL_C) {
+        process.exit();
+    }
+
     if(!started) {
         setTimeout(()=>{
             stdout.clearLine();
             stdout.cursorTo(0);
 
             console.log(`Time's up!`);
-            console.log(`WPM: ${Math.round(corrects/5*(60/givenSeconds))}`);
-            console.log(`All keystrokes: ${keypresses}`);
-            console.log(`Correct keystrokes: ${corrects}`);
-            console.log(`Wrong keystrokes: ${errors}`);
-            console.log(`Accuracy: ${Math.round(corrects/keypresses * 10000)/100}%`);
+            console.log(`WPM: ${Math.round(stats.corrects/5*(60/config.givenSeconds))}`);
+            console.log(`All keystrokes: ${stats.keypresses}`);
+            console.log(`Correct keystrokes: ${stats.corrects}`);
+            console.log(`Wrong keystrokes: ${stats.errors}`);
+            console.log(`Accuracy: ${Math.round(stats.corrects/stats.keypresses * 10000)/100}%`);
             process.exit();
-        }, givenSeconds * 1000);
+        }, config.givenSeconds * 1000);
 
         startTime = Date.now();
         started = true;
-    }
-
-    // exit on ctrl-c
-    if (key == SPECIAL.CTRL_C) {
-        process.exit();
     }
 
     // end of current line
@@ -101,22 +102,22 @@ stdin.on('data', key => {
         // the last char with the colored background takes up 10 characters
         results = results.slice(0, -10);
         cursor--;
+    } else if(!key.match(/[a-zA-Z0-9\s]/)) {
+        return;
     } else {
         wrote += key;
 
         if(key == text[cursor]){
             results += SPECIAL.GREEN_BG;
-            corrects++;
+            stats.corrects++;
         } else {
             results += SPECIAL.RED_BG;
-            errors++;
+            stats.errors++;
         }
         results += text[cursor] + SPECIAL.BG_END;
-
         cursor++;
-        keypresses++;
+        stats.keypresses++;
     }
-
 
     // erease the whole thing and display the updated version
     stdout.clearLine();
